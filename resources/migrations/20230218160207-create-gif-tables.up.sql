5a3771e31ce346991bd4423886a71f274c55a249
Nik Peric <nik@nikperic.com>
2023-04-25 19:34:20 +0200

    rename html to link

--- resources/migrations/20230218160207-create-gif-tables.up.sql
+++ resources/migrations/20230218160207-create-gif-tables.up.sql
@@ -1,7 +1,7 @@
 create table if not exists gifs
 (
     id         serial primary key,
-    html       text                      not null,
+    link       text                      not null,
     name       text                      not null,
     created_at timestamptz default now() not null
 );
